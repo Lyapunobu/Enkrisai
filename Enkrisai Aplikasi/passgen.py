ad4170bb88fec651faa91380d1f4dfb3ce7b9386
Lyapunobu <anaqiahza2006@gmail.com>
2024-11-06 19:31:17 +0700

    code optimization

--- Enkrisai Aplikasi/passgen.py
+++ Enkrisai Aplikasi/passgen.py
@@ -7,6 +7,7 @@ numbers = True
 punctuations = True
 
 def printSetting():
+    print("\nSettings:")
     print(f"1. Uppercase: {uppercase}")
     print(f"2. Lowercase: {lowercase}")
     print(f"3. Numbers: {numbers}")
@@ -28,51 +29,56 @@ def passgen(n):
     
     return ''.join(secrets.choice(charset) for i in range(n))
 
-while True:
-    try:
+def inputPasswordLength():
+    while True:
+        try:
 
-        passwordLength = int(input("Panjang password (8-128): "))
+            passwordLength = int(input("Panjang password (8-128): "))
 
-        if passwordLength < 8 or passwordLength > 128:
+            if passwordLength < 8 or passwordLength > 128:
+                print("Tolong masukkan angka dari 8 sampai dengan 128.\n")
+                continue
+            else:
+                break
+        
+        except ValueError:
             print("Tolong masukkan angka dari 8 sampai dengan 128.\n")
-            continue
-        else:
-            break
-    
-    except ValueError:
-        print("Tolong masukkan angka dari 8 sampai dengan 128.\n")
+    return passwordLength
 
+def settings():
 
+    global uppercase, lowercase, numbers, punctuations
 
-while True:
-    try:
+    while True:
 
-        if uppercase == False and lowercase == False and numbers == False and punctuations == False:
+        if not (uppercase or lowercase or numbers or punctuations):
             print("Semua setting tidak bisa dimatikan!")
             lowercase = True
 
-        print()
         printSetting()
 
-        inputSetting = int(input("\nPilih nomor mana yang ingin dinyalakan/dimatikan ([0] untuk selesai): "))
-        
-        if inputSetting == 1:
-            uppercase = not uppercase
-            continue
-        elif inputSetting == 2:
-            lowercase = not lowercase
-            continue
-        elif inputSetting == 3:
-            numbers = not numbers
-            continue       
-        elif inputSetting == 4:
-            punctuations = not punctuations
-            continue
-        elif inputSetting == 0:
-            break
+        try:
+            inputSetting = int(input("\nPilih nomor mana yang ingin dinyalakan/dimatikan ([0] untuk selesai): "))
+            
+            if inputSetting == 1:
+                uppercase = not uppercase
+                continue
+            elif inputSetting == 2:
+                lowercase = not lowercase
+                continue
+            elif inputSetting == 3:
+                numbers = not numbers
+                continue       
+            elif inputSetting == 4:
+                punctuations = not punctuations
+                continue
+            elif inputSetting == 0:
+                break
+        except ValueError:
+            print("Input tidak valid, tolong masukkan angka.")
 
-    except ValueError:
-        print("Input tidak valid, tolong masukkan angka.")
+passwordLength = inputPasswordLength()
+settings()
 
 print("\nPassword yang sudah dibuat:")
 print(passgen(passwordLength))
