74d197eca7baa4296cc77a2e6d2402906038546f
Lyapunobu <anaqiahza2006@gmail.com>
2025-01-01 21:49:55 +0700

    username generator rework & minor password generator fixes

--- username/usernameGenerator.py
+++ username/usernameGenerator.py
@@ -3,17 +3,71 @@ import random
 namaHewan = "ref/namaHewan.txt"
 kataSifat = "ref/kataSifat.txt"
 
+entity = True
+adjective = True
+numbers = True
+
+def printUserConfig():
+    print("\nSettings:")
+    print(f"1. Kata Entitas: {entity}")
+    print(f"2. Kata Sifat: {adjective}")
+    print(f"3. Nomor: {numbers}")
+
+def usernameConfig():
+
+    global entity, adjective, numbers
+
+    while True:
+        
+        if not (entity or adjective or numbers):
+            print("\nSemua setting tidak bisa dimatikan!")
+            entity = True
+
+        printUserConfig()
+
+        try:
+            inputSetting = int(input("\nPilih nomor mana yang ingin dinyalakan/dimatikan ([0] untuk selesai): "))
+            
+            if inputSetting == 1:
+                entity = not entity
+                continue
+            elif inputSetting == 2:
+                adjective = not adjective
+                continue
+            elif inputSetting == 3:
+                numbers = not numbers
+                continue
+            elif inputSetting == 0:
+                break
+
+        except ValueError:
+            print("Input tidak valid, tolong masukkan angka.")
+
+usernameConfig()
+
 def usernameGenerator():
+    
+    komponenUsername = []
 
-    with open(namaHewan, encoding="utf-8") as f:
-        kata = random.choice(f.read().split()).strip()
+    if entity:
+        with open(namaHewan, encoding="utf-8") as f:
+            kata = random.choice(f.read().split()).strip()
+            komponenUsername.append(kata)
 
-    with open(kataSifat, encoding="utf-8") as f:
-        sifat = random.choice(f.read().split()).strip()
+    if adjective:
+        with open(kataSifat, encoding="utf-8") as f:
+            sifat = random.choice(f.read().split()).strip()
+            komponenUsername.append(sifat)
 
-    angka = random.randint(10,99)
+    if numbers:
+        angka = random.randint(10,99)
+        komponenUsername.append(str(angka))
 
-    return f"{kata}{sifat}{angka}"
+    if not komponenUsername:
+        return "Error! Setting kosong."
 
+    generatedUsername = ''.join(komponenUsername)
 
-print(usernameGenerator())
+    return print(generatedUsername)
+
+usernameGenerator()
