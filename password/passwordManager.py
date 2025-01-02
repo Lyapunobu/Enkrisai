5af5d867e1754d6efa9ad08f805f2611385fa666
Lyapunobu <anaqiahza2006@gmail.com>
2025-01-02 10:12:51 +0700

    minor fix

--- password/passwordManager.py
+++ password/passwordManager.py
@@ -5,24 +5,18 @@ import json
 
 # Mengimpor fungsi dari file lain
 from password.passwordGenerator import passwordGenerator
-from password.passwordGeneratorUnused import passwordGenerator as unused_password_generator
 
 # Fungsi untuk menampilkan menu utama
 def password_menu():
     while True:
         print("\n=== PASSWORD MENU ===")
-        print("1. Generate Password (Using Configured Settings)")
-        print("2. Generate Password (Unused Version)")
-        print("3. Exit to Main Menu")
+        print("1. Generate Password")
+        print("0. Exit to Main Menu")
         choice = input("Pilih opsi: ")
 
         if choice == '1':
             generate_password()
-        elif choice == '2':
-            length = input_password_length()
-            password = unused_password_generator(length)
-            print(f"Password yang dihasilkan (Unused): {password}")
-        elif choice == '3':
+        elif choice == '0':
             print("Kembali ke menu utama.")
             break
         else:
@@ -47,5 +41,5 @@ def generate_password():
     print(f"Password yang sudah dibuat: {password}")
 
 # Fungsi utama
-if __name__ == "__main__":
-    password_menu()
+""" if __name__ == "__main__":
+    password_menu() """
