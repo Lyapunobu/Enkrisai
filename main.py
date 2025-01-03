0b839b54cde33389c65d00f00e78793caad9714c
Lyapunobu <anaqiahza2006@gmail.com>
2025-01-03 14:47:02 +0700

    code cleaaning

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
-# from menu.login_menu import main_menu
-from views.mainMenu import main_menu
+from views.mainMenu import mainMenu
 
 if __name__ == "__main__":
     os.system('cls')
-    main_menu()
+    mainMenu()
 
 # RAAAAAAAAAAAAAAAAAAAAAAAAH
